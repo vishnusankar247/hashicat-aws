d484abf6d568aa0ad5d784f9d42698c785152534
vizsankar <50675571+vishnusankar247@users.noreply.github.com>
2020-09-17 10:12:24 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+t#!/bin/bash
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s app. Where cartoons come to life.
   <!-- END -->
 
   </div>
